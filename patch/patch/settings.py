b207e95dcaa1ada7ffa91213c2b564996d27338d
Filip <filip@vujicic.com>
2017-09-15 09:34:10 +1200

    Disabled debug (was apparently left on)

--- patch/patch/settings.py
+++ patch/patch/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8xgmur-gapivo71bt5*gi7sm=mnf5x9emt4f7oy9q7o=uyz2fn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
